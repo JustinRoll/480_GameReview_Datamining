a6998bda8928b4a094513d42b575f68d0cb01f49
Justin Roll <justin.j.roll@gmail.com>
2015-03-15 12:43:47 -0700

    Added gold standard for clustering. Next to eval cluster purity

---
+++ reviewGoldStandard.py
@@ -0,0 +1,103 @@
+goldStandard = ['AdvanceWars.txt','AdvanceWars2BlackHoleRising.txt'], \
+['All-StarBaseball2003.txt','All-StarBaseball2004.txt', 'MLBSlugfest20-04.txt', 'MajorLeagueBaseball2K7.txt'], \
+['BombermanMax2RedAdvance.txt','BombermanTournament.txt'], \
+['BreathofFire.txt', 'BreathofFireII.txt'], \
+['CastlevaniaAriaofSorrow.txt', 'CastlevaniaCircleoftheMoon.txt', 'CastlevaniaHarmonyofDissonance.txt'], \
+['ClassicNESSeriesTheLegendofZelda.txt', 'ClassicNESSeriesZeldaIITheAdventureofLink.txt', 'TheLegendofZeldaALinktothePast.txt','TheLegendofZeldaTheMinishCap.txt'], \
+['CrashBandicoot2N-Tranced.txt','CrashBandicootPurpleRipto\'sRampage.txt','CrashBandicootTheHugeAdventure.txt'], \
+['DaveMirraFreestyleBMX2.txt', 'DaveMirraFreestyleBMX3.txt'], \
+['DavisCupTennis.txt', 'VirtuaTennis.txt','WTATourTennis.txt'], \
+['DisneySportsBasketball.txt','DisneySportsFootball.txt','DisneySportsSkateboarding.txt','DisneySportsSoccer.txt'], \
+['DonkeyKongCountry.txt', 'DonkeyKongCountry2.txt', 'DonkeyKongCountry3.txt'], \
+['Doom.txt','DoomII.txt'], \
+['DragonBallAdvancedAdventure.txt','DragonBallGTTransformation.txt','DragonBallZBuu\'sFury.txt','DragonBallZSupersonicWarriors.txt','DragonBallZTaiketsu.txt','DragonBallZTheLegacyofGoku.txt','DragonBallZTheLegacyofGokuII.txt'], \
+['ESPNFinalRoundGolf2002.txt', 'TigerWoodsPGATour2004.txt'], \
+['FIFA07Soccer.txt', 'FIFAWorldCupGermany2006.txt'], \
+['FinalFantasyI&IIDawnofSouls.txt', 'FinalFantasyIVAdvance.txt', 'FinalFantasyTacticsAdvance.txt','FinalFantasyVAdvance.txt','FinalFantasyVIAdvance.txt'], \
+['FireEmblem.txt', 'FireEmblemTheSacredStones.txt'], \
+['FireProWrestling.txt', 'FireProWrestling2.txt', 'LegendsofWrestlingII.txt', 'WWERoadtoWrestleManiaX8.txt','WWESurvivorSeries.txt','WWFRoadtoWrestlemania.txt'], \
+['GTAdvance2RallyRacing.txt','GTAdvance3ProConceptRacing.txt','GTAdvanceChampionshipRacing.txt', 'NeedforSpeedCarbonOwntheCity.txt','NeedforSpeedMostWanted.txt','NeedforSpeedUnderground.txt', 'TopGearGTChampionship.txt','TopGearRally.txt'], \
+['GoldenSun.txt', 'GoldenSunTheLostAge.txt'], \
+['HarryPotterQuidditchWorldCup.txt','HarryPotterandtheGobletofFire.txt','HarryPotterandthePrisonerofAzkaban.txt','HarryPotterandtheSorcerer\'sStone.txt'], \
+['HarvestMoonFriendsofMineralTown.txt', 'HarvestMoonMoreFriendsofMineralTown.txt'], \
+['Iridion3D.txt','IridionII.txt'], \
+['Kirby&theAmazingMirror.txt','KirbyNightmareinDreamLand.txt'], \
+['LegoStarWars.txt', 'LegoStarWarsIITheOriginalTrilogy.txt'], \
+['MaddenNFL06.txt','MaddenNFL07.txt','MaddenNFL2003.txt','MaddenNFL2004.txt','MaddenNFL2005.txt', 'NFLBlitz20-02.txt'], \
+['Mario&LuigiSuperstarSaga.txt', 'MarioGolfAdvanceTour.txt', 'MarioKartSuperCircuit.txt', 'MarioPartyAdvance.txt', 'MarioPinballLand.txt','MarioTennisPowerTour.txt','SuperMarioAdvance.txt','SuperMarioAdvance4SuperMarioBros.3.txt','SuperMarioWorldSuperMarioAdvance2.txt','Yoshi\'sIslandSuperMarioAdvance3.txt','YoshiTopsy-Turvy.txt'], \
+['MarvelUltimateAlliance.txt', 'TheIncredibleHulk(2003).txt', 'TheInvincibleIronMan.txt', 'X-MenReignofApocalypse.txt','X-MenTheOfficialGame.txt'], \
+['MatHoffman\'sProBMX.txt','MatHoffman\'sProBMX2.txt'], \
+['MedalofHonorInfiltrator.txt', 'MedalofHonorUnderground.txt'], \
+['MegaMan&Bass.txt',
+'MegaManBattleChipChallenge.txt',
+'MegaManBattleNetwork.txt',
+'MegaManBattleNetwork2.txt',
+'MegaManBattleNetwork3Blue.txt',
+'MegaManBattleNetwork3White.txt',
+'MegaManBattleNetwork4BlueMoon.txt',
+'MegaManBattleNetwork4RedSun.txt',
+'MegaManBattleNetwork5TeamColonel.txt',
+'MegaManBattleNetwork5TeamProtoman.txt',
+'MegaManBattleNetwork6CybeastFalzar.txt',
+'MegaManBattleNetwork6CybeastGregar.txt',
+'MegaManZero.txt',
+'MegaManZero2.txt',
+'MegaManZero3.txt',
+'MegaManZero4.txt'], \
+['MetroidFusion.txt','MetroidZeroMission.txt'], \
+['MortalKombatAdvance.txt',
+'MortalKombatDeadlyAlliance.txt',
+'MortalKombatTournamentEdition.txt'], \
+['NamcoMuseum.txt',
+'NamcoMuseum50thAnniversary.txt'], \
+['PiratesoftheCaribbeanDeadMan\'sChest.txt','PiratesoftheCaribbeanTheCurseoftheBlackPearl.txt'], \
+['PitfallTheLostExpedition.txt','PitfallTheMayanAdventure.txt'], \
+['PokemonEmerald.txt',
+'PokemonFireRedVersion.txt',
+'PokemonLeafGreenVersion.txt',
+'PokemonMysteryDungeonRedRescueTeam.txt',
+'PokemonPinballRuby&Sapphire.txt',
+'PokemonRubyVersion.txt',
+'PokemonSapphireVersion.txt'], \
+['Rayman3.txt',
+'RaymanAdvance.txt',
+'RaymanHoodlum\'sRevenge.txt', 'RaymanRavingRabbids.txt'], \
+['SSX3.txt','SSXTricky.txt'], \
+['SegaArcadeGallery.txt',
+'SegaSmashPack.txt'], \
+['ShiningSoul.txt',
+'ShiningSoulII.txt'], \
+['SonicAdvance.txt',
+'SonicAdvance2.txt',
+'SonicAdvance3.txt',
+'SonicBattle.txt',
+'SonicPinballParty.txt',
+'SonictheHedgehogGenesis.txt'], \
+['Spider-Man2.txt',
+'Spider-Man3.txt',
+'Spider-ManMysterio\'sMenace.txt',
+'Spider-ManTheMovie.txt'], \
+['SpyroOrangeTheCortexConspiracy.txt',
+'SpyroSeasonofIce.txt', 'TheLegendofSpyroANewBeginning.txt'], \
+['StarWarsEpisodeIIAttackoftheClones.txt',
+'StarWarsTheNewDroidArmy.txt',
+'StarWarsTrilogyApprenticeoftheForce.txt'], \
+['SummonNightSwordcraftStory.txt',
+'SummonNightSwordcraftStory2.txt'], \
+['SuperRobotTaisenOriginalGeneration.txt',
+'SuperRobotTaisenOriginalGeneration2.txt'], \
+['TMNT.txt', 'TeenageMutantNinjaTurtles.txt', 'TeenageMutantNinjaTurtles2BattleNexus.txt'], \
+['TheLordoftheRingsTheFellowshipoftheRing.txt','TheLordoftheRingsTheReturnoftheKing.txt','TheLordoftheRingsTheTwoTowers.txt'], \
+['TheSimsBustin\'Out.txt',
+'TheUrbzSimsintheCity.txt'], \
+['TonyHawk\'sDownhillJam.txt','TonyHawk\'sProSkater2.txt','TonyHawk\'sProSkater3.txt','TonyHawk\'sProSkater4.txt','TonyHawk\'sUnderground.txt'], \
+['WarioLand4.txt',
+'WarioWareInc.MegaMicroGame$!.txt',
+'WarioWareTwisted!.txt'], \
+['Yu-Gi-Oh!WorldChampionshipTournament2004.txt',
+'Yu-Gi-Oh!WorldwideEditionStairwaytotheDestinedDuel.txt']
+
+goldStandardList = []
+for category in goldStandard:
+	for game in category:
+		goldStandardList.append(game)
